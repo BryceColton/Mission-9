744d13fc55a7c81c716cc467e430aaf6c7d5a6d9
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-12 10:52:38 -0600

    adding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Welcome() {
+  return (<h1>Criminally Underrated Bands</h1>)
+}
+
+const bandNames = [
+  {
+    name: "Dire Straights", 
+    members: "R.E.M", 
+    formed: 1977},
+  {name: "R. E. M", 
+    members: "Michael Stripe, Peter Buck, Mike Willis, Billy Berry", 
+    formed: 1980},
+  {name: "Collective Soul", 
+    members: "Michael Stripe, Peter Buck, Mike Willis, Billy Berry", 
+    formed: 1992},
+  {
+    name: 'Traffic',
+    members: 'Steve Wimwood, Jim Capaldi, Chris Wood, Dave Mason',
+    formed: 1923
+  }
+]
+
+function Band({name, members, formed}: {name: string, members: string, formed: number}) {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <img />
+    <h2>Name: {name}</h2>
+    <h3>Original Members: {members}</h3>
+    <h3>Formed: {formed}</h3>
+    </>
+  )
+}
+
+function BandList() {
+
+  return (
+    <>
+        {
+          bandNames.map((singleBand) => {
+            <Band {...singleBand} />
+          })
+        }
+    </>
+
+
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+      <Welcome />
+      <BandList/>
+
     </>
   )
 }
