6850af69c03520b56d67be348d47626996a5470f
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-12 11:15:50 -0600

    message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,34 @@
 
 import './App.css'
+import teamsObject from './CollegeBasketballTeams.json'
 
 function Welcome() {
+
   return (<h1>Criminally Underrated Bands</h1>)
 }
 
-const bandNames = [
-  {
-    name: "Dire Straights", 
-    members: "R.E.M", 
-    formed: 1977},
-  {name: "R. E. M", 
-    members: "Michael Stripe, Peter Buck, Mike Willis, Billy Berry", 
-    formed: 1980},
-  {name: "Collective Soul", 
-    members: "Michael Stripe, Peter Buck, Mike Willis, Billy Berry", 
-    formed: 1992},
-  {
-    name: 'Traffic',
-    members: 'Steve Wimwood, Jim Capaldi, Chris Wood, Dave Mason',
-    formed: 1923
-  }
-]
-
-function Band({name, members, formed}: {name: string, members: string, formed: number}) {
+function Team({school, name, city, state}: {school: string, name: string, city: string, state: string}) {
 
   return (
     <>
     <img />
-    <h2>Name: {name}</h2>
-    <h3>Original Members: {members}</h3>
-    <h3>Formed: {formed}</h3>
+    <h2>School: {school}</h2>
+    <h3>Mascot Name: {name}</h3>
+    <h3>City and State: {city}</h3>
+    <h3>State: {state}</h3>
+
+
     </>
   )
 }
 
-function BandList() {
+function TeamList() {
 
   return (
     <>
-        {
-          bandNames.map((singleBand) => {
-            <Band {...singleBand} />
-          })
-        }
+        {teamsObject.teams.map((team, index) => (
+        <Team key={index} {...team} />
+      ))}
     </>
 
 
@@ -56,7 +41,7 @@ function App() {
   return (
     <>
       <Welcome />
-      <BandList/>
+      <TeamList />
 
     </>
   )
